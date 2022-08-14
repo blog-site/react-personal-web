9d38eb8e3ce04000fec29716af6076d29e32f25e
ycwang <ycwang.cs@gmail.com>
2022-08-14 17:06:28 +0800

    react markdown

--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -1,8 +1,10 @@
 import ArchiveStyle from './Archive.module.scss';
+import ReactMarkdown from 'react-markdown'
 function Archive() {
   return (
     <div className={ArchiveStyle.Archive}>
       <h2>Archive</h2>
+      <ReactMarkdown children={'# This is a header\n\nAnd this is a paragraph'} />
     </div>
   );
 }
