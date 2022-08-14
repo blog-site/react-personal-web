d484a12ce213a7f59b2eb3273c652bfdd07250a1
ycwang <ycwang.cs@gmail.com>
2022-08-14 17:33:35 +0800

    file markdown

--- src/pages/Archive.js
+++ src/pages/Archive.js
@@ -1,10 +1,20 @@
+import React from 'react';
 import ArchiveStyle from './Archive.module.scss';
-import ReactMarkdown from 'react-markdown'
+import ReactMarkdown from 'react-markdown';
+
 function Archive() {
+  const [content, setContent] = React.useState();
+  const file = require('../asset/archives/content/test1.md');
+  fetch(file)
+    .then((response) => response.text())
+    .then((text) => {
+      setContent(text);
+    });
+
   return (
     <div className={ArchiveStyle.Archive}>
       <h2>Archive</h2>
-      <ReactMarkdown children={'# This is a header\n\nAnd this is a paragraph'} />
+      <ReactMarkdown children={content || '# Loading...'} />
     </div>
   );
 }
