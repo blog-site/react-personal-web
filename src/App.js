b703ee861a00c82376c47cd9c87aafd2c2534ac2
ycwang <ycwang.cs@gmail.com>
2022-08-14 16:11:10 +0800

    sass config

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import logo from './logo.svg';
-import './App.css';
+import AppStyle from './App.module.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={AppStyle.App}>
+      <header className={AppStyle.header}>
+        <img src={logo} className={AppStyle.logo} alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className={AppStyle.link}
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
