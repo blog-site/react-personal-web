45563f5ffb844cea7e468d75dae8c800a4ebb141
ycwang <ycwang@debian.tonych.me>
2022-09-10 14:40:44 +0800

    style for show archives edit

--- src/components/archive/ShowArchivesEdit.js
+++ src/components/archive/ShowArchivesEdit.js
@@ -1,8 +1,12 @@
+import ShowArchivesEditStyle from './ShowArchivesEdit.module.scss';
+
 import { Link } from 'react-router-dom';
 
 import { getArchives } from '../../actions';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
+
+import moment from 'moment';
 
 function ShowArchivesEdit() {
   const archive_list = useSelector(
@@ -20,20 +24,48 @@ function ShowArchivesEdit() {
   }, [archive_list_state, dispatch]);
 
   return (
-    archive_list === null ?
-      [] :
-      archive_list.map(
-        (archive) => (
-          <div key={archive.slug}>
-            <Link to={`/archive/${archive.slug}`}>
-              <h2>{archive.title}</h2>
-            </Link>
-            <Link to={`/admin/update-archive/${archive.slug}`}>
-              Edit
-            </Link>
-          </div>
+    <Archives archive_list={archive_list} />
+  );
+}
+
+function Archives(props) {
+  let _props = props;
+  let archive_list = _props.archive_list;
+  if (Object.keys(archive_list).length === 0) {
+    return [];
+  }
+  return (
+    <div className={ShowArchivesEditStyle.Archives}>
+      {
+        archive_list.map(
+          (archive) => (
+            <Archive key={archive.slug} archive={archive} />
+          )
         )
-      )
+      }
+    </div>
+  );
+}
+
+function Archive(props) {
+  let _props = props;
+  let archive = _props.archive;
+  var date_published = moment(archive.date_published).format('YYYY-MM-DD');
+  return (
+    <div className={ShowArchivesEditStyle.Archive}>
+      <div className={ShowArchivesEditStyle.ArchiveLine}>
+        <Link to={`/archive/${archive.slug}`}>
+          <h2>{archive.title}</h2>
+        </Link>
+        <Link to={`/admin/update-archive/${archive.slug}`}>
+          <p>Edit</p>
+        </Link>
+      </div>
+      <div className={ShowArchivesEditStyle.ArchiveLine}>
+        <h4>{archive.subtitle}</h4>
+        <p>published on: {date_published}</p>
+      </div>
+    </div>
   );
 }
 
