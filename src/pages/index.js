fc9b30684743a4a408a4b2376d554ebde74b260f
ycwang <ycwang@debian.tonych.me>
2022-09-14 20:20:52 +0800

    login page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import Home from './Home';
 import About from './About';
+import Login from './admin/Login';
 import Admin from './admin/Admin';
 import UpdateArchive from './admin/UpdateArchive';
 import Archives from './archives/Archives';
 import Archive from './archives/Archive';
 import NoMatch from './NoMatch';
-export { Home, About, Admin, UpdateArchive, Archives, Archive, NoMatch };
+export { Home, About, Login, Admin, UpdateArchive, Archives, Archive, NoMatch };
