41d3f98d79237d1a4b762e3c8bdd3f9aae316b99
ycwang <ycwang@debian.tonych.me>
2022-09-16 22:57:01 +0800

    fix archives epics source file

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,6 +1,6 @@
 import { combineEpics } from 'redux-observable';
 import AuthEpics from './auth';
-import ArchivesEpics from './archive/archive';
+import ArchivesEpics from './archive/archives';
 import ArchiveEpics from './archive/archive';
 
 export default combineEpics(AuthEpics, ArchivesEpics, ArchiveEpics);
